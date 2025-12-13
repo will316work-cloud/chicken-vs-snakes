818c696ddf7d3123c39e39318ef50a7c37b6b64d
William <will316.work@gmail.com>
2025-12-13 03:34:06 -0800

    Added Spawn Enemies

---
+++ Assets/Scripts/Spawn Enemies.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Spawners;
+
+namespace ChickenSnakes.EnemyGroup
+{
+    public class SpawnEnemies : MonoBehaviour
+    {
+        #region Serialized Fields
+
+
+        [SerializeField] private Vector2Int _dimensions;
+        [SerializeField] private Vector2 _space;
+        [SerializeField] private EnemyPort _enemyPort;
+
+
+        #endregion
+
+        #region Private Fields
+
+
+        private Dictionary<Vector2Int, EnemyPort> _positionsAndEnemies = new Dictionary<Vector2Int, EnemyPort>();
+
+
+        #endregion
+
+        #region Public Methods
+
+
+        [ContextMenu("Clear Enemies")]
+        public void ClearEnemies()
+        {
+            foreach (EnemyPort port in _positionsAndEnemies.Values)
+            {
+                port.Return();
+
+                ObjectPoolManager.ReturnObjectToPool(port.gameObject);
+            }
+
+            _positionsAndEnemies.Clear();
+        }
+
+        [ContextMenu("Spawn Group")]
+        public void SpawnGroup()
+        {
+            if (_positionsAndEnemies.Count > 0)
+            {
+                ClearEnemies();
+            }
+
+            float horizontalDisplacement = -_dimensions.x / 2 + (_dimensions.x % 2 == 0 ? 0.5f : 0);
+            float verticalDisplacement = 0.5f;
+
+            for (int i = 0; i < _dimensions.x; i++)
+            {
+                for (int j = 0; j < _dimensions.y; j++)
+                {
+                    Vector2Int pos = new Vector2Int(i, j);
+
+                    EnemyPort spawnedObject = ObjectPoolManager.SpawnObject(_enemyPort, Vector3.zero, Quaternion.identity);
+                    spawnedObject.transform.SetParent(transform);
+                    _positionsAndEnemies.Add(pos, spawnedObject);
+
+                    Vector3 realLocalPosition = (Vector3Int)pos;
+                    realLocalPosition.x += horizontalDisplacement;
+                    realLocalPosition.y += verticalDisplacement;
+
+                    realLocalPosition.x *= _space.x;
+                    realLocalPosition.y *= _space.y;
+
+                    spawnedObject.transform.localPosition = realLocalPosition;
+                    spawnedObject.transform.localRotation = Quaternion.Euler(0, 0, 90);
+                }
+            }
+        }
+
+
+        #endregion
+    }
+}
